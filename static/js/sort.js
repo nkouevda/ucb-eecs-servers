96d751f5c48d02e1e374d6ceb46d444c2bf60678
Nikita Kouevda <nkouevda@gmail.com>
2013-04-09 02:47:14 -0700

    Add arrows to indicate sorting.

--- static/js/sort.js
+++ static/js/sort.js
@@ -1,5 +1,5 @@
 // Nikita Kouevda
-// 2013/04/08
+// 2013/04/09
 
 // Sort by the first element of the given pairs
 var sortPairsNumerical = function (a, b) {
@@ -8,10 +8,10 @@ var sortPairsNumerical = function (a, b) {
 
 // Sort the table of servers by the given column
 var sortServerTable = function (column) {
-  var table = document.getElementById('servers');
-  var th = table.rows[0], online = [], offline = [];
+  var table = document.getElementById('servers').tBodies[0];
+  var online = [], offline = [];
 
-  for (var i = 1, len = table.rows.length; i < len; ++i) {
+  for (var i = 0, len = table.rows.length; i < len; ++i) {
     var row = table.rows[i];
     var content = row.cells[column].textContent || row.cells[column].innerText;
 
