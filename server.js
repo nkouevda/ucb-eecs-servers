3d9991dbda3dbc0652f7274860d0ce1e11f250c3
Nikita Kouevda <nkouevda@gmail.com>
2013-04-10 01:17:44 -0700

    Update asynchronously.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Nikita Kouevda
-// 2013/04/09
+// 2013/04/10
 
 // Required libraries
 var express = require('express');
@@ -7,50 +7,64 @@ var ejs = require('ejs');
 var fs = require('fs');
 var exec = require('child_process').exec;
 
-// Instantiate the app and initialize its default contents
-var app = express();
+// Initialize the default contents to be served
 var onlineServers = [], offlineServers = [];
 var title = 'Current Status of EECS Servers';
 var baseSubtitle = 'Updated every 5 minutes; last updated at ', subtitle = '';
-var best_hive = '';
+var bestHive = '';
 
 // Retrieve the current information
 var loadFiles = function () {
-  // Read the online and offline files
-  var online = fs.readFileSync(__dirname + '/data/online.txt', 'utf-8');
-  var offline = fs.readFileSync(__dirname + '/data/offline.txt', 'utf-8');
+  // Update the online server list
+  fs.readFile(__dirname + '/data/online.txt', 'utf-8', function (err, data) {
+    if (err) {
+      throw err;
+    } else {
+      onlineServers = data.trim().split('\n').map(function (server) {
+        var info = server.toString().split(',');
 
-  // Split the content into arrays of objects
-  onlineServers = online.trim().split('\n').map(function (server) {
-    var info = server.toString().split(',');
-
-    return {
-      online: true,
-      name: info[0],
-      users: info[1],
-      load: info[2]
-    };
+        return {
+          online: true,
+          name: info[0],
+          users: info[1],
+          load: info[2]
+        };
+      });
+    }
   });
-  offlineServers = offline.trim().split('\n').map(function (server) {
-    return {
-      online: false,
-      name: server,
-      users: '',
-      load: ''
-    };
+
+  // Update the offline server list
+  fs.readFile(__dirname + '/data/offline.txt', 'utf-8', function (err, data) {
+    if (err) {
+      throw err;
+    } else {
+      offlineServers = data.trim().split('\n').map(function (server) {
+        return {
+          online: false,
+          name: server,
+          users: '',
+          load: ''
+        };
+      });
+    }
   });
 
   // Retrieve the modification time and update the subtitle
-  var mtime = fs.statSync(__dirname + '/data/online.txt').mtime;
-  subtitle = baseSubtitle + mtime.toString().match(/\d+:\d+:\d+/);
+  fs.stat(__dirname + '/data/online.txt', 'utf-8', function (err, stats) {
+    if (err) {
+      throw err;
+    } else {
+      subtitle = baseSubtitle + stats.mtime.toString().match(/\d+:\d+:\d+/);
+    }
+  });
 
   // Load the new best hive
   fs.readFile(__dirname + '/data/best_hive.txt', 'utf-8', function (err, data) {
     if (err) {
       throw err;
+    } else {
+      bestHive = data.trim();
     }
-
-    best_hive = data;
   });
 };
 
@@ -61,10 +75,6 @@ loadFiles();
 (function updateFiles() {
   // Run the update script
   exec(__dirname + '/bin/update.sh', function (err, stdout, stderr) {
-    if (err) {
-      throw err;
-    }
-
     if (stdout) {
       console.log(stdout);
     }
@@ -73,18 +83,26 @@ loadFiles();
       console.error(stderr);
     }
 
-    // Load the new information
-    loadFiles();
+    if (err) {
+      throw err;
+    } else {
+      // Load the new information
+      loadFiles();
+    }
   });
 
   // Repeat every 5 minutes
   setTimeout(updateFiles, 1000 * 60 * 5);
 })();
 
-// Register the engine, set the views directory path, and default to .ejs files
+// Instantiate the app and set up ejs
+var app = express();
 app.engine('html', ejs.__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+// Serve files in the static directory statically
+app.use('/static', express.static(__dirname + '/static'));
 
 // Serve index.ejs at the root
 app.get('/', function (req, res) {
@@ -97,12 +115,9 @@ app.get('/', function (req, res) {
   });
 });
 
-// Serve files in the static directory statically
-app.use('/static', express.static(__dirname + '/static'));
-
 // Serve a plaintext version of the least busy hive server
 app.get('/best_hive', function (req, res) {
-  res.send(best_hive);
+  res.send(bestHive);
 });
 
 // Listen on the given port, defaulting to 3000
