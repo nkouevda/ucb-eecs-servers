1c842a10581e408f3370bd777cd84e7df340b220
Nikita Kouevda <nkouevda@gmail.com>
2013-06-28 20:26:06 -0700

    Use placeholders for configuration values.

--- bin/config.sh
+++ bin/config.sh
@@ -2,7 +2,7 @@
 # 2013/06/28
 
 # User, server, and options for ssh
-user="cs188-je"
-server="hive7.cs.berkeley.edu"
-ssh_opts=("-o ConnectTimeout=3" "-o ServerAliveInterval=3")
+user="USERNAME"
+server="SERVER"
+ssh_opts=("-oConnectTimeout=3" "-oServerAliveInterval=3")
 remote_dir="ucb-eecs-servers"
