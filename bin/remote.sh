0ab5999b77d1efb25096875a515f3bf793a6c356
Nikita Kouevda <nkouevda@gmail.com>
2013-08-14 23:05:50 -0700

    Quote variable assignment.

--- bin/remote.sh
+++ bin/remote.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Nikita Kouevda
-# 2013/06/30
+# 2013/08/14
 
 # Number of users online
-users=$(who | wc -l | awk '{print $1}')
+users="$(who | wc -l | awk '{print $1}')"
 
 # Load average for past 5 minutes
-load=$(uptime | perl -pe 's/.+://; s/,//g' | awk '{print $2}')
+load="$(uptime | perl -pe 's/.+://; s/,//g' | awk '{print $2}')"
 
 # Output information as csv
 echo "$1,$users,$load"
