a31f2a28fc528baf639c16b23621612f6bbf26e9
Nikita Kouevda <nkouevda@gmail.com>
2013-09-20 16:33:26 -0700

    Fix variable name.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Nikita Kouevda
-# 2013/07/17
+# 2013/09/20
 
 # Change directory to parent directory of location of script
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
@@ -10,7 +10,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 . "bin/settings.sh"
 
 # Make the base directory if necessary
-ssh "${ssh_opts[@]}" "$username@$server" "mkdir -p $remote_dir"
+ssh "${ssh_config[@]}" "$username@$server" "mkdir -p $remote_dir"
 
 # Copy the necessary files over
 scp -r "${ssh_config[@]}" {bin,data} "$username@$server:$remote_dir/"
